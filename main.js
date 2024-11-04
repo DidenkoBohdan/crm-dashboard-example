9325ffba3cf9db4e6e9a4541ba4f78609bc0fdac
Bohdan Didenko <bohdandidenko@gmail.com>
2024-11-03 20:22:13 -0800

    fix: removed logs

--- main.js
+++ main.js
@@ -5,16 +5,13 @@ const mediaQuery = window.matchMedia("(max-width: 1280px)");
 
 mediaQuery.onchange = (e) => {
   if (e.matches) {
-    console.log('matches on resize');
     toggleNavBarInit();
   } else {
-    console.log('not match')
   }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   if (mediaQuery.matches) {
-    console.log('matches on load');
     toggleNavBarInit();
   }
 });
